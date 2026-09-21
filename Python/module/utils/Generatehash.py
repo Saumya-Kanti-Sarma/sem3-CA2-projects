f833af5c6561e1bfaae49d96f31f1044bfd0b3f0
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-09-21 08:45:49 +0530

    feat: added nonce updating function with a proper hash_standards

--- Python/module/utils/Generatehash.py
+++ Python/module/utils/Generatehash.py
@@ -1,12 +1,20 @@
 import hashlib
 
-def generate_hash(index, transaction, previous_hash, timestamp, nonce):
-        block_data = "-".join([str(index), str(transaction), str(previous_hash), str(timestamp), str(nonce)])
+def generate_hash(index, transaction, previous_hash, timestamp, hash_standard="00ec0"):
+        nonce = 0
+        initial_hash_letters = ""
+        hash_digest = ""
+        while initial_hash_letters != hash_standard:
+                block_data = "-".join([str(index), str(transaction), str(previous_hash), str(timestamp), str(nonce)])
+                hash_object = hashlib.sha256()
+                hash_object.update(block_data.encode('utf-8'))
 
-        hash_object = hashlib.sha256()
-        hash_object.update(block_data.encode('utf-8'))
+                hash_digest = hash_object.hexdigest()
+                initial_hash_letters = hash_digest[:len(hash_standard)]
+                # print(initial_hash_letters)
+                nonce +=1
+        return hash_digest
 
-        return hash_object.hexdigest()
 
 if __name__ == "__main__":
-  print(generate_hash(1,100,000000000000,92749228.523842,4829))
+  print(generate_hash(1,100,000000000000,92749228.523842))
